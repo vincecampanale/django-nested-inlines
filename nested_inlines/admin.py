6f9df1e9edf88c75d095e88fc2e0fe319293833d
Vince Campanale <vcampanale0294@gmail.com>
2020-01-02 21:55:00 -0500

    Fix import

--- nested_inlines/admin.py
+++ nested_inlines/admin.py
@@ -2,7 +2,7 @@ from django import VERSION as DJANGO_VERSION
 from django.contrib.admin.options import (ModelAdmin, InlineModelAdmin,
     csrf_protect_m, models, transaction, all_valid,
     PermissionDenied, unquote, reverse)
-from django.utils.safestring import get_func_args
+from django.utils.inspect import get_func_args
 
 # Maintain backwards compatibility
 try:
